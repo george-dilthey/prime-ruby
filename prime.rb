64a13d7d6db6bed54d1af98dbfaeceba1ef84226
George Dilthey <george.dilthey@gmail.com>
2021-02-07 00:24:46 -0500

    commit tree c9989616bd2f3294fbdeac9441ee81c97d3ca236

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(num)
+    if num == 2
+        return true
+    elsif num <= 1
+        return false
+    elsif num.even?
+        return false
+    else
+        (2..num-1).to_a.all? do |i| 
+            num % i != 0
+        end
+    end
+end
